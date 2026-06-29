d678338eb19b1f4e25bdd426f4d504ba8e63fe9e
yyq <2780764346@qq.com>
2026-06-29 17:12:14 +0800

    fix: switch to @astrojs/vercel adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
